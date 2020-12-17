211912268a533ef64786d8e3ee3585347a38e1fa
Hughes <mushhughes@cannonschool.org>
2020-12-17 09:04:04 -0500

    tagged deven in heckleDeven

--- responses/heckleDeven.js
+++ responses/heckleDeven.js
@@ -4,7 +4,7 @@ module.exports = {
 		const reactEmojis = [];
 		if (message.member.user.id == 689939738911965198) {
 			if(Math.floor(Math.random() * 50) == 	0) {
-				return 'You have good Ana mechanics and aim you just need to work on positioning.';
+				return 'You have good Ana mechanics and aim, <@689939738911965198>, you just need to work on positioning.';
 			}
 		}
 
