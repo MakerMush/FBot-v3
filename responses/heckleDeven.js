ab6566df219673a00eae0440063db42a310db782
Hughes <mushhughes@cannonschool.org>
2020-12-17 08:52:55 -0500

    heckleDeven

---
+++ responses/heckleDeven.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'heckleDeven',
+	execute(message) {
+		const reactEmojis = [];
+		if (message.member.user.id == 689939738911965198) {
+			if(Math.floor(Math.random() * 50) == 	0) {
+				return 'You have good Ana mechanics and aim you just need to work on positioning.';
+			}
+		}
+
+	},
+};
