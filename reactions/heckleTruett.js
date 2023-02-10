cdbb03adfe3f0b24d92c1755d4e6416d5a4fb36e
Mush Hughes <mchughescannon@gmail.com>
2023-02-10 10:32:54 -0500

    added HT

---
+++ reactions/heckleTruett.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'heckleTruett',
+	execute(message) {
+		const reactEmojis = [];
+		if (message.member.user.id == 1026599284075921419) {
+			reactEmojis.push('😐');
+		}
+
+		// return reactEmojis;
+	},
+};
